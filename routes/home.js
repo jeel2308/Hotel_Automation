96af1ce1d87881a03e1c1a5ab51ea3d6e4ef3785
Arex <arexnj24@gmail.com>
2020-06-03 19:56:03 +0530

    updated 2

--- routes/home.js
+++ routes/home.js
@@ -1,13 +1,28 @@
 const router = require('express').Router();
 
-router.get("/",function(req,res){
+router.get("/home",function(req,res){
     const loggedIn = !!req.isAuthenticated();
     res.render("home",{loggedIn});
 });
 
 router.get("/about-us",function(req,res){
     const loggedIn = !!req.isAuthenticated();
-    res.render("about_us",{loggedIn});
+    res.render("about-us",{loggedIn});
+});
+
+router.get("/profile",function(req,res){
+    const loggedIn = !!req.isAuthenticated();
+    res.render("profile",{loggedIn});
+});
+
+router.get("/register",function(req,res){
+    const loggedIn = !!req.isAuthenticated();
+    res.render("sign-up",{loggedIn});
+});
+
+router.get("/login",function(req,res){
+    const loggedIn = !!req.isAuthenticated();
+    res.render("sign-in",{loggedIn});
 });
 
 module.exports = router;
