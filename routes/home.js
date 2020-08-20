e4d9b54e5dcf67a929576f062eb12b81a289b16b
jeel2308 <jeelpatel238@gmail.com>
2020-08-20 23:50:21 +0530

    integrated device controller with website and ui improvement

--- routes/home.js
+++ routes/home.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const Booking = require("../models/Booking");
 
 router.get("/",function(req,res){
     const loggedIn = !!req.isAuthenticated();
@@ -10,9 +11,25 @@ router.get("/about-us",function(req,res){
     res.render("about-us",{loggedIn});
 });
 
-router.get("/appliances",function(req,res){
+router.get("/appliances",async function(req,res){
     const loggedIn = !!req.isAuthenticated();
-    res.render("appliances",{loggedIn});
+    if(loggedIn){
+        const roomPromise = new Promise(function(resolve,reject){
+            Booking.find({username : req.user.username},function(err,rooms){
+                if(err) reject(err);
+                else resolve(rooms);
+            });
+        });
+        try{
+            const rooms = await roomPromise;
+            if(!rooms.length) res.redirect("/book-room");
+            else res.render("appliances",{loggedIn});
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+    else res.redirect("/sign-in");
 });
 
 module.exports = router;
